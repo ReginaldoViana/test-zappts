1dbfeed44c3a77cce122bdc43f24743e105b3548
Reginaldo Viana <reginaldo_stz@outlook.com>
2020-07-28 15:50:56 -0300

    teste-Zappts

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,50 @@
+import { trigger, style, state, transition, animate } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.sass'],
+  animations: [
+    trigger('switchPage', [
+      state('up', style({
+        height: '83px',
+        opacity: 1
+      })),
+      state('in', style({
+        height: 0,
+        opacity: 0
+      })),
+      transition('up => in', [
+        animate('0.6s')
+      ]),
+      transition('in => up', [
+        animate('0.6s')
+      ]),
+    ])
+  ]
+})
+export class FormComponent implements OnInit {
+
+  formGroup: FormGroup;
+
+  isSignUp = false;
+
+  constructor(
+    private _fb: FormBuilder
+  ) {
+    this.formGroup = this._fb.group({
+      fullName: [null, Validators.required],
+      userNameEmail: [null, Validators.required],
+      password: [null, Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  isValid(form: FormControl | any) {
+    return form.invalid && form.touched;
+  }
+
+}
